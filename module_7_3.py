17e446c36df82ed45fd4426ad265bf730053e28c
Pasha110788 <bodzin1988@gmail.com>
2024-10-09 13:15:54 +0300

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,38 @@
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                text = file.read()
+                text = text.lower()
+                text = text.translate({ord(i): None for i in '.,=!?;:-'})
+                words = text.split()
+                all_words[file_name] = words
+        return all_words
+
+    def find(self, word):
+        word = word.lower()
+        result = {}
+        all_words = self.get_all_words()
+        for file_name, words in all_words.items():
+            if word in words:
+                result[file_name] = words.index(word) + 1
+        return result
+
+    def count(self, word):
+        word = word.lower()
+        result = {}
+
+        all_words = self.get_all_words()
+        for file_name, words in all_words.items():
+            result[file_name] = words.count(word)
+        return result
+
+
+finder1 = WordsFinder('Mother Goose - Monday’s Child.txt',)
+print(finder1.get_all_words())
+print(finder1.find('Child'))
+print(finder1.count('Child'))
